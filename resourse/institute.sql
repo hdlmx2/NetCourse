2b031e2f27327ccf714e9f3a87f8f9ebee13d9cf
handong <handong@DESKTOP-HVATGE1>
2017-05-12 21:28:33 +0800

    学院和专业表的insert 语句

---
+++ resourse/institute.sql
@@ -0,0 +1,12 @@
+insert into tb_institute values(1,'计算机科学与工程学院');
+insert into tb_institute values(2,'人文学院');
+insert into tb_institute values(3,'外国语学院');
+insert into tb_institute values(4,'艺术与服装工程学院');
+insert into tb_institute values(5,'电气与自动化工程学院');
+insert into tb_institute values(6,'数学与统计学院');
+insert into tb_institute values(7,'化学与材料工程学院');
+insert into tb_institute values(8,'机械工程学院');
+insert into tb_institute values(9,'物理与电子工程学院');
+insert into tb_institute values(10,'经济与管理学院');
+
+commit
---
+++ resourse/major.sql
@@ -0,0 +1,56 @@
+-- ----
+-- 计算机学院
+-- ---
+insert into tb_major values(101,'计算机科学与技术',1);
+insert into tb_major values(102,'软件工程',1);
+insert into tb_major values(103,'物联网工程',1);
+insert into tb_major values(104,'网络工程',1);
+-- 人文学院
+insert into tb_major values(201,'汉语言文学',2);
+insert into tb_major values(202,'秘书学',2);
+insert into tb_major values(203,'历史学',2);
+-- 外国语学院
+insert into tb_major values(301,'英语',3);
+insert into tb_major values(302,'日语',3);
+insert into tb_major values(303,'朝鲜语',3);
+insert into tb_major values(304,'德语',3);
+-- 艺术与服装工程学院
+insert into tb_major values(401,'服装设计',4);
+insert into tb_major values(402,'环境设计',4);
+insert into tb_major values(403,'数码艺术',4);
+insert into tb_major values(404,'美术学',4);
+-- 电气与自动化工程学院
+insert into tb_major values(501,'自动化',5);
+insert into tb_major values(502,'电气工程',5);
+
+-- 数学与统计学院
+insert into tb_major values(601,'应用数学',6);
+insert into tb_major values(602,'统计数学',6);
+insert into tb_major values(603,'信息与计算科学',6);
+
+-- 化学与材料工程学院
+insert into tb_major values(701,'应用化学',7);
+insert into tb_major values(702,'高分子材料',7);
+insert into tb_major values(703,'金属材料工程',7);
+insert into tb_major values(704,'无机功能材料',7);
+
+-- 机械工程学院
+insert into tb_major values(801,'机械工程',8);
+insert into tb_major values(802,'汽车服务工程',8);
+insert into tb_major values(803,'机械电子工程',8);
+insert into tb_major values(804,'材料成型与控制工程',8);
+
+-- 物理与电子工程学院
+insert into tb_major values(901,'物理学',9);
+insert into tb_major values(902,'电子科学与技术',9);
+insert into tb_major values(903,'光电信息科学与工程 ',9);
+insert into tb_major values(904,'新能源科学与工程',9);
+
+-- 经济与管理学院
+insert into tb_major values(1001,'公共事业管理 ',10);
+insert into tb_major values(1002,'文化产业管理',10);
+insert into tb_major values(1003,'市场营销 ',10);
+insert into tb_major values(1004,'财务管理',10);
+insert into tb_major values(1005,'经济与金融 ',10);
+insert into tb_major values(1006,'旅游管理',10);
+insert into tb_major values(1007,'酒店管理',10);
