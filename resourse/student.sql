268c9c3da5a04f17377a95fe3d7eab81a04e24c7
handong <handong@DESKTOP-HVATGE1>
2017-05-13 08:49:51 +0800

    添加student和teacher sql

---
+++ resourse/student.sql
@@ -0,0 +1,4 @@
+insert into tb_student values('9904898713','123456',101,'韩','东','990489713@qq.com','image/teacher_01.jpg');
+commit;
+
+insert into tb_student values('957962992','123456',102,'穆','昕','957962992@qq.com','image/teacher_01.jpg');
---
+++ resourse/teacher.sql
@@ -0,0 +1,22 @@
+use netcourse;
+insert into tb_teacher values('t1001','12345',1,'翁','恺');
+insert into tb_teacher values('t1002','12345',1,'车','万年');
+insert into tb_teacher values('t1003','12345',1,'战','德成');
+insert into tb_teacher values('t1004','12345',1,'张','丽杰');
+insert into tb_teacher values('t1005','12345',1,'崔','毅东');
+insert into tb_teacher values('t1006','12345',1,'唐','大士');
+insert into tb_teacher values('t1007','12345',1,'陈','越');
+insert into tb_teacher values('t1008','12345',1,'刘','宏伟');
+insert into tb_teacher values('t1009','12345',1,'蔡','郭阳');
+insert into tb_teacher values('t1010','12345',1,'范','红军');
+insert into tb_teacher values('t1011','12345',1,'孟','宁');
+insert into tb_teacher values('t1012','12345',1,'华','保健');
+insert into tb_teacher values('t1013','12345',1,'李','全龙');
+insert into tb_teacher values('t1014','12345',1,'陈','岭');
+insert into tb_teacher values('t1015','12345',1,'秦','静');
+insert into tb_teacher values('t1016','12345',1,'陈','振宇');
+insert into tb_teacher values('t1017','12345',1,'朱','少明');
+insert into tb_teacher values('t1018','12345',1,'金','须亮');
+
+
+select * from tb_teacher;
