792d1164540f3a764770b2280a6e29b163a740e6
handong <handong@DESKTOP-HVATGE1>
2017-05-13 14:27:36 +0800

    添加了video sql

---
+++ resourse/video.sql
@@ -0,0 +1,16 @@
+use netcourse;
+
+insert into tb_video values(101001001,'安装编程软件',101001,'source/cs/Java程序设计/1.安装编程软件.mp4');
+insert into tb_video values(101001002,'Mac OS上的eclipse',101001,'source/cs/Java程序设计/1.2.Mac OS上的eclipse.mp4');
+insert into tb_video values(101001003,'第一个C语言程序',101001,'source/cs/Java程序设计/3.第一个C语言程序：hello world.mp4');
+insert into tb_video values(101001004,'命令行如何编译程序',101001,'source/cs/Java程序设计/4.命令行如何编译程序.mp4');
+insert into tb_video values(101001005,'做点计算',101001,'source/cs/Java程序设计/5.做点计算.mp4');
+insert into tb_video values(101001006,'输入',101001,'source/cs/Java程序设计/6.输入.mp4');
+insert into tb_video values(101001007,'变量赋值',101001,'source/cs/Java程序设计/7.变量赋值.mp4');
+insert into tb_video values(101001008,'赋值',101001,'source/cs/Java程序设计/8.赋值.mp4');
+insert into tb_video values(101001009,'浮点数',101001,'source/cs/Java程序设计/9.浮点数.mp4');
+insert into tb_video values(101001010,'常量和变量',101001,'source/cs/Java程序设计/10.常量和变量.mp4');
+insert into tb_video values(101001011,'类型转换',101001,'source/cs/Java程序设计/11.类型转换.mp4');
+insert into tb_video values(101001012,'变量',101001,'source/cs/Java程序设计/12.变量.mp4');
+insert into tb_video values(101001013,'定义变量',101001,'source/cs/Java程序设计/13.定义变量.mp4');
+insert into tb_video values(101001014,'第一个Java程序',101001,'source/cs/Java程序设计/14.第一个Java程序-Hello World.mp4');
